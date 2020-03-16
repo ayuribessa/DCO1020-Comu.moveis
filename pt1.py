ca9b6c8e155e813522d553dd55e61759ca96ad07
Ayuri Bessa <ayuribessa@gmail.com>
2020-03-16 20:27:39 -0300

    changes

--- pt1.py
+++ pt1.py
@@ -16,35 +16,49 @@ AHM = 3.2*(math.log10(11.75*HMOB))**2 - 4.97
 
 def main():
 
-    #Chamando a função que calcula os centros dos hexagonos
+    #1)Chamando a função que calcula os centros dos hexagonos
     centros = calcCentros(XGRID,YGRID)
-    #Chamando função que plota os 7 hexagonos num grid
+    #2)Chamando função que plota os 7 hexagonos num grid
     MakeGrid(centros)
+    #Plotando os centros em cima do Grid
     plt.scatter(np.real(centros),np.imag(centros))
     #Mostrando o plot
-    plt.show()
+    #plt.show()
 
-    #Criando o grid de pontos de medição para cada Bs
+    #3)Criando o grid de pontos de medição para cada Bs
     xx,yy,posEachBs = calcPontosMedicao(XGRID,YGRID,centros)
     plt.scatter(np.real(centros),np.imag(centros))
      
     #Mostrando os pontos de medição em cima do grid para as 7 Erbs
     printPontosMedicao(posEachBs,centros)
+    #4)Criando a matriz de potencias de cada ERB
     mtPotEachBsdBm = calcMatrizPotenciasBsdbm(posEachBs)
+    #5)Mostra a REMs das 7 ERBS
     printREMs(xx,yy,mtPotEachBsdBm,centros)
+    calcShowMatrizPotenciasAll(mtPotEachBsdBm,xx,yy,centros)
 
-
+#função que calcula a malha de pontos de medição em cima do grid de hexagonos
 def calcPontosMedicao(XGRID,YGRID,centros):
     passo = math.ceil(RAIO/10)
     XGRID += (XGRID % passo)
     YGRID += (YGRID % passo)
-    xx, yy = np.meshgrid( np.arange(0,XGRID,passo), np.arange(0,YGRID,passo) )
+    #xx, yy = np.meshgrid( np.arange(0,XGRID,passo), np.arange(0,YGRID,passo) )
+    xx, yy = np.meshgrid( np.linspace(0,XGRID,passo), np.linspace(0,YGRID,passo) )
     posEachBs = []
     for i in range(7):
         posBs = (xx + yy*1j) - centros[i]
         posEachBs.append(posBs)
     posEachBs = np.array(posEachBs)
     return xx,yy,posEachBs
+
+def calcShowMatrizPotenciasAll(mtPotEachBsdBm,xx,yy,centros):
+    MtPotenciasDbmFinal = np.NINF*np.ones(np.shape(YGRID))
+    for i in range(7):
+        MtPotenciasDbmFinal = np.maximum(MtPotenciasDbmFinal,mtPotEachBsdBm[i])
+    plt.pcolor(xx,yy,MtPotenciasDbmFinal,vmax=-115,vmin=-55)
+    plt.colorbar()
+    MakeGrid(centros)
+    plt.axis('equal')
 
 def calcMatrizPotenciasBsdbm(posEachBs):
     mtDisEachBsNorm = np.abs(posEachBs)
@@ -62,19 +76,22 @@ def printREMs(xx,yy,mtPotEachBsdBm,centros):
         plt.autoscale()
         plt.show()
 
+
 def printPontosMedicao(posEachBs,centros):
     for i in range(7):
-        plt.scatter(posEachBs[i].real, posEachBs[i].imag)
+        plt.scatter(np.real(posEachBs[i]), np.imag(posEachBs[i]))
         plt.title('ERB {}'.format(i+1))
         MakeGrid(centros - centros[i])
-        plt.autoscale()
+        plt.axis('equal')
         plt.show()
 
 
+
+#Essa é a função que desenha o(s) hexagonos
 def makeHex(centro):
     PontosHex = []
     for i in range(7):
-        ponto = RAIO*np.exp(np.pi/3*(i-1)*1j)
+        ponto = RAIO*np.exp(np.pi/3*i*1j)
         PontosHex.append(ponto)
 
     pontosHex = np.asarray(PontosHex)
@@ -85,7 +102,11 @@ def makeHex(centro):
     #plt.scatter(centro.real,centro.imag, marker="s")
     #plt.show()
 
+#Função que calcula os centros do 7 hexagonos do Grid
+#Tem como entrada o tamanho do eixo x e y do Grid desejado
 def calcCentros(XGRID,YGRID):
+    #Centros inicia com um elemento sendo zero porque ao somar com o tamanho do grid/2 posterirmente
+    #o primento elemento já vai ser o centro do hexagono central
     Centros = [0] 
     for a in range(6):
         centro = RAIO*np.sqrt(3)*np.exp(((a-2)*np.pi/3 + OFFSET)*1j)
@@ -97,6 +118,8 @@ def calcCentros(XGRID,YGRID):
     return centros
 
 #Função para desenhar o grid de hexagonos
+#Chama a função que desenha o hexagono o n° de vezes igual ao n° de elementos do array centros
+#O array centros é um array de números complexos que são os centros dos 7 hexagonos
 def MakeGrid(centros):
     for point in centros:
         makeHex(point)
