d30a82555f61f3286e6325106cd98d69ec74c4ff
Ayuri Bessa <ayuribessa@gmail.com>
2020-03-21 14:04:42 -0300

    finished pt 1

--- pt1_complots.py
+++ pt1_complots.py
@@ -4,7 +4,7 @@ import math
 
 #constantes
 
-RAIO = 5e3
+RAIO = 5e3 
 XGRID = 5*RAIO
 YGRID = 6*np.sqrt(3/4)*RAIO
 OFFSET = np.pi/6
@@ -13,6 +13,7 @@ PTDBM = 57
 HMOB = 5
 FC = 800
 AHM = 3.2*(math.log10(11.75*HMOB))**2 - 4.97
+SIGMA_SHADOW = 8
 
 def main():
 
@@ -21,13 +22,13 @@ def main():
     #2)Chamando função que plota os 7 hexagonos num grid
     MakeGrid(centros)
     #Plotando os centros em cima do Grid
-    plt.scatter(np.real(centros),np.imag(centros))
+    plt.scatter(centros.real , centros.imag)
     #Mostrando o plot
-    #plt.show()
+    plt.show()
 
     #3)Criando o grid de pontos de medição para cada Bs
     xx,yy,posEachBs = calcPontosMedicao(XGRID,YGRID,centros)
-    plt.scatter(np.real(centros),np.imag(centros))
+    #plt.scatter(np.real(centros),np.imag(centros))
      
     #Mostrando os pontos de medição em cima do grid para as 7 Erbs
     printPontosMedicao(posEachBs,centros)
@@ -36,10 +37,9 @@ def main():
     #5)Mostra a REMs das 7 ERBS
     printREMs(xx,yy,mtPotEachBsdBm,centros)
     calcShowMatrizPotenciasAll(mtPotEachBsdBm,xx,yy,centros)
-
 #função que calcula a malha de pontos de medição em cima do grid de hexagonos
 def calcPontosMedicao(XGRID,YGRID,centros):
-    passo = math.ceil(RAIO/10)
+    passo = math.ceil(RAIO/20)
     XGRID += (XGRID % passo)
     YGRID += (YGRID % passo)
     #xx, yy = np.meshgrid( np.arange(0,XGRID,passo), np.arange(0,YGRID,passo) )
@@ -51,6 +51,9 @@ def calcPontosMedicao(XGRID,YGRID,centros):
     posEachBs = np.array(posEachBs)
     return xx,yy,posEachBs
 
+def calcMtShadow():
+    MtPtShadowDbmFinal = np.NINF*np.ones(np.shape(YGRID))
+
 def calcShowMatrizPotenciasAll(mtPotEachBsdBm,xx,yy,centros):
     MtPotenciasDbmFinal = np.NINF*np.ones(np.shape(YGRID))
     for i in range(7):
@@ -58,12 +61,13 @@ def calcShowMatrizPotenciasAll(mtPotEachBsdBm,xx,yy,centros):
     plt.pcolor(xx,yy,MtPotenciasDbmFinal,vmax=-115,vmin=-55)
     plt.colorbar()
     MakeGrid(centros)
-    plt.axis('equal')
+    #plt.axis('equal')
+    plt.show()
 
 def calcMatrizPotenciasBsdbm(posEachBs):
     mtDisEachBsNorm = np.abs(posEachBs)
     np.where(mtDisEachBsNorm < RAIO, RAIO, mtDisEachBsNorm)
-    mtPldb = 69.55+26.6*math.log10(FC)+(44.9-6.55*math.log10(HBSS))*np.log10(mtDisEachBsNorm/1e3) - 13.82*math.log10(HBSS) - AHM
+    mtPldb = 69.55+26.16*math.log10(FC)+(44.9-6.55*math.log10(HBSS))*np.log10(mtDisEachBsNorm/1e3) - 13.82*math.log10(HBSS) - AHM
     mtPotEachBsdBm = PTDBM - mtPldb
     return mtPotEachBsdBm
 
@@ -79,10 +83,10 @@ def printREMs(xx,yy,mtPotEachBsdBm,centros):
 
 def printPontosMedicao(posEachBs,centros):
     for i in range(7):
-        plt.scatter(np.real(posEachBs[i]), np.imag(posEachBs[i]))
+        plt.scatter((posEachBs[i].real), (posEachBs[i].imag))
         plt.title('ERB {}'.format(i+1))
         MakeGrid(centros - centros[i])
-        plt.axis('equal')
+        #plt.axis('equal')
         plt.show()
 
 
@@ -93,7 +97,6 @@ def makeHex(centro):
     for i in range(7):
         ponto = RAIO*np.exp(np.pi/3*i*1j)
         PontosHex.append(ponto)
-
     pontosHex = np.asarray(PontosHex)
     pontosHex += centro
     X = np.real(pontosHex)
@@ -109,10 +112,8 @@ def calcCentros(XGRID,YGRID):
     #o primento elemento já vai ser o centro do hexagono central
     Centros = [0] 
     for a in range(6):
-        centro = RAIO*np.sqrt(3)*np.exp(((a-2)*np.pi/3 + OFFSET)*1j)
+        centro = RAIO*np.sqrt(3)*np.exp((a*np.pi/3 + OFFSET)*1j)
         Centros.append(centro)
-    
-
     centros = np.asarray(Centros)
     centros += complex(XGRID/2,YGRID/2)
     return centros
--- pt1_final_outage2modelos.py
+++ pt1_final_outage2modelos.py
@@ -18,7 +18,7 @@ def main():
     print('Aguarde um pouco...')
     vFreqs = np.array([800, 900, 1800, 1900, 2100])
     for freq in vFreqs:
-        raio = 3000
+        raio = 3000 #valor definido para ser o inicial do raio
         while True:
             flag = 0
             xgrid = 5*raio
@@ -33,7 +33,7 @@ def main():
             flag = calcOutage(choice,raio, freq, posEachBs, ygrid)
             if(flag != 0):
                 break
-            raio = raio + 1
+            raio = raio + 5
 
 
 def calcCentros(raio, xgrid, ygrid):
@@ -71,37 +71,37 @@ def calcMatrizPotenciasAll(mtPotEachBsdBm, ygrid):
 
     MtPotenciasDbmFinal = np.NINF*np.ones(np.shape(ygrid))
     for i in range(7):
-        MtPotenciasDbmFinal = np.maximum(
-            MtPotenciasDbmFinal, mtPotEachBsdBm[i])
+        MtPotenciasDbmFinal = np.maximum(MtPotenciasDbmFinal, mtPotEachBsdBm[i])
     return MtPotenciasDbmFinal
 
 
 
 def calcOutage(choice,raio, freq, posEachBs, ygrid):
    
+    #ahm = ( 1.1*math.log10(fc) - 0.7) * HMOB - (1.56*math.log10(fc)-0.8) ahm para  cost na literatura
+    ahm = 3.2*np.log10(11.75*HMOB)**2 - 4.97
     if  choice == 1: 
-        mtPotEachBsdBm = calcMtPtBsdbmOkHata(raio, posEachBs, freq)
+        mtPotEachBsdBm = calcMtPtBsdbmOkHata(raio, posEachBs, freq, ahm)
     else:
-        mtPotEachBsdBm = calcMtPtBsdbmCost231(raio, posEachBs, freq)
+        mtPotEachBsdBm = calcMtPtBsdbmCost231(raio, posEachBs, freq, ahm)
     MtPotenciasDbmFinal = calcMatrizPotenciasAll(mtPotEachBsdBm, ygrid)
     x = ((MtPotenciasDbmFinal < SENSIBILIDADE).sum())
     y = np.size(MtPotenciasDbmFinal)
     outage = x/y
 
-    if outage >= 0.09999:
+    if outage >= 0.0999998:
         printValues(freq,raio,outage)
         return 1
     return 0
 
 def printValues(freq,raio,outage):
-    print('---------------------')
-    print('Com a frequencia da portadora: {:.2f} mHz'.format(freq))
+    print('------------------------------------------------------')
+    print('Com a frequencia da portadora: {:d} MHz'.format(freq))
     print('O maior raio que deu taxa de outage com até 10% foi: {:.2f} m '.format(raio))
-    print('Com outage calculado de: {:.2%}'.format(outage))
+    print('Com outage calculado de: {:.4%}'.format(outage))
 
-def calcMtPtBsdbmOkHata(raio, posEachBs, fc):
+def calcMtPtBsdbmOkHata(raio, posEachBs, fc, ahm):
         
-    ahm = 3.2*np.log10(11.75*HMOB)**2 - 4.97
     mtDisEachBsNorm = np.abs(posEachBs)
     np.where(mtDisEachBsNorm < raio, raio, mtDisEachBsNorm)
     mtPldb = 69.55 + 26.16 * math.log10(fc) + (44.9-6.55*math.log10(HBSS))*np.log10(mtDisEachBsNorm/1e3) - \
@@ -109,9 +109,8 @@ def calcMtPtBsdbmOkHata(raio, posEachBs, fc):
     mtPotEachBsdBm = PTDBM - mtPldb
     return mtPotEachBsdBm
 
-def calcMtPtBsdbmCost231(raio, posEachBs, fc):
+def calcMtPtBsdbmCost231(raio, posEachBs, fc,ahm):
 
-    ahm = ( 1.1*np.log10(fc) - 0.7) * HMOB - (1.56*np.log10(fc)-0.8)
     mtDisEachBsNorm = np.abs(posEachBs)
     np.where(mtDisEachBsNorm < raio, raio, mtDisEachBsNorm)
     mtPldb = 46.3 + 33.9*math.log10(fc) - 13.82*math.log10(HBSS) - ahm + \
@@ -122,7 +121,7 @@ def calcMtPtBsdbmCost231(raio, posEachBs, fc):
 
 def getChoice():
     while True:
-        print('Deseja saber outage para Modelo de Okomura Hata (1) ou' +
+        print('Deseja saber outage para Modelo de Okomura Hata (1) ou ' +
         'Cost 231 (2) ? ')
         choice = int(input('Digite <1> ou <2> '))
         if choice == 1 or choice == 2:
